8f5a7e7344988dba1b4547697ac7255d8c436f30
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-22 00:36:00 +0600

    bannar added

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -4,7 +4,9 @@ import BannerImg from '../../images/banner/banner2.png'
 const Banner = () => {
     return (
         <div>
-            
+            <div>
+                <img src={BannerImg} alt="" />
+            </div>
         </div>
     );
 };
