5413bc18e2bc4088881ce648bc267f6018bdd14e
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-22 03:01:39 +0600

    tools adde

--- src/Pages/Tool/Tool.js
+++ src/Pages/Tool/Tool.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Tool = () => {
+const Tool = ({ tool }) => {
+    const {name, img, description, price, Minimum, available} = tool;
     return (
-        <div>
-            
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={img} alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">{name}</h2>
+                <p>{description}</p>
+                <p><small>Price: ${price}</small></p>
+                <p><small>Minimum order Quantity: {Minimum}</small></p>
+                <p><small>Available Quantity: {available}</small></p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Tools/Tools.js
+++ src/Pages/Tools/Tools.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useTools from '../../hooks/useTool';
+import Tool from '../Tool/Tool';
 
 const Tools = () => {
+    const [tools, setTools] = useTools();
     return (
         <div>
-            
+           <h4>Tools</h4> 
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+               {
+                  tools.slice(0, 6).map(tool=><Tool
+                  key={tool._id}
+                  tool={tool}
+                  ></Tool>) 
+               }
+           </div>
         </div>
     );
 };
