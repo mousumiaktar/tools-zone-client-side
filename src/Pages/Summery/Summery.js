1e9df2713fa9a71c4c8d52732bb46c928f5f192e
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-22 11:54:19 +0600

    summery added

---
+++ src/Pages/Summery/Summery.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Summery.css';
+
+const Summery = () => {
+    return (
+        <div className='mt-10 mb-10'>
+            <h2 className='text-xl text-purple-500 text-center font-bold'>MILLIONS BUSINESS TRUST US</h2>
+            <p className='text-center'>TRY TO UNDERSTAND USERS EXPECTATION</p>
+            <div class="divider">Or</div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'  id="summery-container">
+                <div>
+                    <h3>72</h3>
+                    <p>Countries</p>
+                </div>
+                <div>
+                    <h3>535+</h3>
+                    <p>Complete Projects</p>
+                </div>
+                <div>
+                    <h3>273+</h3>
+                    <p>Happy Clients</p>
+                </div>
+                <div>
+                    <h3>432+</h3>
+                    <p>Feedbacks</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Summery;
